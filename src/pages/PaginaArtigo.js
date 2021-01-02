dfa206eb70180287c496588c4acee9fdae1a6953
Marcelo Brito <marcelogbrito@gmail.com>
2021-01-02 10:58:52 -0300

    use effect para setar quantidade de curtidas

--- src/pages/PaginaArtigo.js
+++ src/pages/PaginaArtigo.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import ListaArtigos from '../components/ListaArtigos';
-import conteudoArtigo from './ConteudoArtigo'
-import PaginaNaoEncontrada from './PaginaNaoEncontrada';
+import React, { useState, useEffect } from "react";
+import ListaArtigos from "../components/ListaArtigos";
+import conteudoArtigo from "./ConteudoArtigo";
+import PaginaNaoEncontrada from "./PaginaNaoEncontrada";
 
-const [informacoesArtigo, setInformacoesArtigo] =    useState({curtidas:0, comentarios: []}) 
+const PaginaArtigo = ({ match }) => {
+  const [informacoesArtigo, setInformacoesArtigo] = useState({
+    curtidas: 0,
+    comentarios: [],
+  });
 
-const PaginaArtigo = ({match}) => {
-    const nome = match.params.nome
-    const artigo =  conteudoArtigo.find(artigo => artigo.nome === nome)
-    if(!artigo) return <PaginaNaoEncontrada />
-    const outrosArtigos = conteudoArtigo.filter(artigo => artigo.nome !== nome)
+  useEffect(() => {
+    setInformacoesArtigo({
+      curtidas: 3,
+    });
+  });
+  const nome = match.params.nome;
+  const artigo = conteudoArtigo.find((artigo) => artigo.nome === nome);
+  if (!artigo) return <PaginaNaoEncontrada />;
+  const outrosArtigos = conteudoArtigo.filter((artigo) => artigo.nome !== nome);
 
-
-    return (
-        <div>
-            <>
+  return (
+    <div>
+      <>
         <h1>{artigo.titulo}</h1>
         <p>Este artigo foi curtido {informacoesArtigo.curtidas} vezes</p>
         {artigo.conteudo.map((paragrafo, key) => (
-            <p key={key}>{paragrafo}</p>
+          <p key={key}>{paragrafo}</p>
         ))}
         <h3>Outros Artigos:</h3>
         <ListaArtigos artigos={outrosArtigos} />
-        
-
-
-    </>
-        </div>
-    );
+      </>
+    </div>
+  );
 };
-
 
 export default PaginaArtigo;
